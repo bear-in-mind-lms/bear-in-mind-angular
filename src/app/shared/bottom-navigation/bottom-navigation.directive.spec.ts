5604e50daf341fed4fdb0823682d21cd81794e01
kaj <hi@kajetansuchanski.pl>
2023-11-19 12:21:05 +0100

    Bottom navigation directive created

---
+++ src/app/shared/bottom-navigation/bottom-navigation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BottomNavigationDirective } from './bottom-navigation.directive';
+
+describe('BottomNavigationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BottomNavigationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
