8d80f60aafa55a15be980bc806cfb6c1e1a41e97
kaj <hi@kajetansuchanski.pl>
2023-12-29 13:50:58 +0100

    API error handling fixed (#6)

--- src/app/api/api-error.ts
+++ src/app/api/api-error.ts
@@ -7,9 +7,10 @@ const ERROR_CODE_CANNOT_ENROLL = 'CANNOT_ENROLL';
 const ERROR_CODE_CANNOT_SIGN_UP_FOR_GROUP = 'CANNOT_SIGN_UP_FOR_GROUP';
 const ERROR_CODE_CONNECTION_FAILED = 'CONNECTION_FAILED';
 const ERROR_CODE_INCORRECT_CREDENTIALS = 'INCORRECT_CREDENTIALS';
+const ERROR_CODE_MODULE_DISABLED = 'MODULE_DISABLED';
 export const ERROR_CODE_NOT_FOUND = 'NOT_FOUND';
 const ERROR_CODE_UNEXPECTED = 'UNEXPECTED';
-const ERROR_CODE_MODULE_DISABLED = 'MODULE_DISABLED';
+const ERROR_CODE_USER_EXISTS = 'USER_EXISTS';
 
 export const connectionFailedError: ApiError = {
   code: ERROR_CODE_CONNECTION_FAILED,
@@ -43,6 +44,9 @@ export function getApiErrorMessage(error: ApiError) {
     }
     case ERROR_CODE_NOT_FOUND: {
       return $localize`:@@errorResourceNotFound:`;
+    }
+    case ERROR_CODE_USER_EXISTS: {
+      return $localize`:@@errorUserAlreadyExists:`;
     }
     case ERROR_CODE_UNEXPECTED:
     default: {
